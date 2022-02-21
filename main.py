a395ee6f2c10c9a152ab61e3fe628892b5b215bd
sami-amer <59567234+samiatMIT@users.noreply.github.com>
2022-02-20 23:23:07 -0500

    added catch for some errors, and some documentation

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 import json
 import logging
 
+## Logging Setup
 logging.basicConfig(filename='AUDIT_LOG.log', level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
 logger = logging.getLogger()
 ch = logging.StreamHandler()
@@ -28,8 +29,14 @@ def bearer_oauth(r):
     return r
 
 def connect_to_endpoint(url,params=None):
+    """
+    Connnects to the Twitter API using the given url and params
+
+    Arguments:
+        url     (str): the url genereated by a function 
+        params  (str): parameters that need to be passed with the url [optional]
+    """
     response = requests.request("GET", url, auth=bearer_oauth,params=params)
-    print(response.status_code)
     if response.status_code != 200:
         raise Exception(
             "Request returned an error: {} {}".format(
@@ -41,6 +48,9 @@ def connect_to_endpoint(url,params=None):
 def get_user_id(user):
     """
     Given a twitter username without "@", returns the user id
+
+    Arguments:
+        user    (str): the twitter username without '@'
     """
     # Specify the usernames that you want to lookup below
     # You can enter up to 100 comma-separated values.
@@ -63,7 +73,13 @@ def get_user_id(user):
     }
     return info
 
-def get_user_timeline(user_id):
+def get_user_timeline(user_id): # ! is this variable in size?
+    """
+    Given a user id, return some tweets from the users timeline
+
+    Arguments:
+        user_id (str): the users id
+    """
     url= "https://api.twitter.com/2/users/{}/tweets".format(user_id)
     params = {"tweet.fields": "text,source,author_id,attachments"}
     data = connect_to_endpoint(url, params)['data'][0] # ? Make this a default dict?
@@ -76,7 +92,7 @@ def get_user_timeline(user_id):
     }
     return info
 
-def get_stream_rules():
+def get_stream_rules(): # ! finish documentation
     response = requests.get(
         "https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth
     )
@@ -84,7 +100,7 @@ def get_stream_rules():
         raise Exception(
             "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
         )
-    # print(json.dumps(response.json()))
+    logging.debug(f"Rule Get Response: {json.dumps(response.json())}")
     try:
         data = response.json()["data"]
         meta = response.json()["meta"]
@@ -98,11 +114,11 @@ def get_stream_rules():
         return None, response.json()
 
 
-def delete_stream_rules(rules):
-    if rules is None or "data" not in rules:
+def delete_all_stream_rules(rules_response):
+    if rules_response is None or "data" not in rules_response:
         return None
 
-    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    ids = list(map(lambda rule: rule["id"], rules_response["data"]))
     payload = {"delete": {"ids": ids}}
     response = requests.post(
         "https://api.twitter.com/2/tweets/search/stream/rules",
@@ -118,7 +134,23 @@ def delete_stream_rules(rules):
 
     logging.debug(f"Rule Deletion Response: {json.dumps(response.json())}")
 
-def set_stream_rules(rules): # ! add a catch for duplicate rules error
+def delete_stream_rules(ids):
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+
+    logging.debug(f"Rule Deletion Response: {json.dumps(response.json())}")
+
+def set_stream_rules(rules):
     payload = {"add":rules}
     response = requests.post(
         "https://api.twitter.com/2/tweets/search/stream/rules",
@@ -131,14 +163,24 @@ def set_stream_rules(rules): # ! add a catch for duplicate rules error
         )
     
     logging.debug(f"Rule Addition Respone: {json.dumps(response.json())}")
-    
-    info = {
-        "rules":response.json()["data"],
-        "created":response.json()["meta"]["summary"]["created"],
-        "valid":response.json()["meta"]["summary"]["valid"],
-    }
 
-    return info
+    try:
+        if response.json()["errors"]:
+            logging.warning("setting rule returned an error: see log file for full response")
+            for num,error in enumerate(response.json()["errors"]):
+                logging.warning(f"Error_{num} Title: {error['title']}")
+                logging.warning(f"Error_{num} Value: {error['value']}")
+                logging.warning(f"Error_{num} id: {error['id']}")
+            
+            return None
+    except KeyError:
+        info = {
+            "rules":response.json()["data"],
+            "created":response.json()["meta"]["summary"]["created"],
+            "valid":response.json()["meta"]["summary"]["valid"],
+        }
+
+        return info
 
 def connect_to_stream():
     response = requests.get(
@@ -161,4 +203,14 @@ def connect_to_stream():
 if __name__ == '__main__':
     # sami_id = get_user_id("Sami_Amer_PS")["id"]
     # sami_tweets = get_user_timeline(sami_id)
-    connect_to_stream()
+    # connect_to_stream()
+
+    rules = [
+        {"value": "from:2899773086"},
+        {"value": "from:Wario64"},
+        {"value": "from:763099930487615488 "}]
+    
+    # rules, response = get_stream_rules()
+    # delete_all_stream_rules(response)
+    
+    # set_stream_rules(rules)
