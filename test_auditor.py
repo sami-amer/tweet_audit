e5a178d268b5c2bd0502542d0268d969d5b4d8d4
sami-amer <59567234+samiatMIT@users.noreply.github.com>
2022-02-27 16:24:15 -0500

    mend

--- test_auditor.py
+++ test_auditor.py
@@ -1,30 +1,38 @@
-import pytest, os
+import pytest, os, logging
 from classes import TweetStream, Tweet, TweetDB, TwitterHandler
-from unittest import patch
+
+formatter = logging.Formatter('%(asctime)s [%(name)s][%(levelname)s] %(message)s')
+log_tester = logging.getLogger("Tester")
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+
+fh_tester = logging.FileHandler("logs/TESTER_LOG.log")
+fh_tester.setFormatter(formatter)
+log_tester.addHandler(fh_tester)
+log_tester.addHandler(ch)
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
-
-
 bearer_token = os.environ.get("BEARER_TOKEN")
-# stream = TweetStream(bearer_token)
+
 class TestHandler:
-    
+    # Uses 4 API Calls total
     def test_get_user_id(self):
         # This test asserts using my twitter account
         # Uses 1 API Call
-        handler = TwitterHandler(bearer_token, None)
+        handler = TwitterHandler(bearer_token, log_tester)
         sami_id = "1441875943136448513"
         assert handler.get_user_id("Sami_Amer_PS")["id"] == sami_id
 
     def test_rules_are_active(self):
         # Uses 1 API Call
-        handler = TwitterHandler(bearer_token, None)
+        handler = TwitterHandler(bearer_token, log_tester)
         assert handler.get_rules()[0]
 
     def test_get_user_from_tweet(self):
         # Uses 1 API Call
-        handler = TwitterHandler(bearer_token, None)
+        handler = TwitterHandler(bearer_token, log_tester)
         tweet_id = "1443636712928915459"
         user_id = "1327422172818788353"
         assert handler.get_user_from_tweet(tweet_id) == user_id 
@@ -40,4 +48,3 @@ class TestTweetClass:
         tweet = Tweet("123456","some Text")
         tweet.set_author_id(testId)
         assert tweet.author_id == testId
-
