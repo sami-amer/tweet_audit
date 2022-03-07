c487517bd36e5ad0e74e1c84c41b0e28faee21cc
sami-amer <59567234+samiatMIT@users.noreply.github.com>
2022-03-06 22:51:53 -0500

    refactored, moved methods from handler to ToolKit

--- tools.py
+++ tools.py
@@ -115,7 +115,69 @@ class Toolkit:
     def update_user_group_db(self,users,table_name,dp_path):
         pass
 
-    
+    def get_user_id(self,user:str) -> dict:
+        """
+        Given a twitter username without "@", returns the user id
+
+        Arguments:
+            user    (str): the twitter username without '@'
+        """
+        # Specify the usernames that you want to lookup below
+        # You can enter up to 100 comma-separated values.
+        usernames = f"usernames={user}"
+        user_fields = "user.fields=id,verified,description,created_at"
+        # User fields are adjustable, options include:
+        # created_at, description, entities, id, location, name,
+        # pinned_tweet_id, profile_image_url, protected,
+        # public_metrics, url, username, verified, and withheld
+        url = "https://api.twitter.com/2/users/by?{}&{}".format(usernames, user_fields)
+        data = self.handler.get_from_endpoint(url)['data'][0]
+
+        info = {
+            "id": data['id'],
+            "name": data['name'],
+            "username": data["username"],
+            "description": data['description'],
+            "verified": data['verified'],
+            "creation_date": data['created_at']
+        }
+        return info
+
+    def get_user_timeline(self,user_id:str,max_results=5) -> list[dict]:
+        """
+        Given a user id, return some tweets from the users timeline
+
+        Arguments:
+            user_id (str): the users id
+        """
+        url= "https://api.twitter.com/2/users/{}/tweets".format(user_id)
+        params = {"tweet.fields": "text,source,author_id,attachments","max_results":str(max_results)}
+        data = self.handler.get_from_endpoint(url, params)['data'] # ? Make this a default dict?
+        tweets = []
+        for tweet in data:
+            info = {
+                "id" : tweet["id"],
+                "text": tweet["text"],
+                "author_id": tweet["author_id"], # ! Add check for this
+                "source": tweet["source"],
+                #"attachments": tweet["attachments"] if tweet["attachments"] else None ! add check for this
+            }
+            tweets.append(info)
+        return tweets
+
+    def get_user_from_tweet(self,id: str):
+        tweet_fields = "tweet.fields=lang,author_id"
+        # ids = "ids=1278747501642657792,1255542774432063488"
+        id = f"ids={id}"
+        url = "https://api.twitter.com/2/tweets?{}&{}".format(id, tweet_fields) # ? Maybe use [text] response to double checK?
+
+        data = self.handler.get_from_endpoint(url)
+        user_id = data["data"][0]["author_id"]
+
+        # print(json.dumps(data, indent=4, sort_keys=True))
+        self.logger.info(f"User ID Query Returned: {user_id}")
+        return user_id
+
 if __name__ == '__main__':
     # --- Code to load pickle and usernames from df
     # with open("_data/pickles/rule_dict","r") as f:
